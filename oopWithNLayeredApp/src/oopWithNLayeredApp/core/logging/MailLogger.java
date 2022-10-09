d272c49a09f02cbdb3fa99dfec5e1079074fe0bd
atagundogan <Atagun3566>
2022-10-10 01:13:13 +0300

    oopWithNLayeredApp

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/core/logging/MailLogger.java
@@ -0,0 +1,13 @@
+package oopWithNLayeredApp.core.logging;
+
+public class MailLogger implements Logger {
+
+	@Override
+	public void log(String data) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mail Gönderildi :" + data);
+		
+	}
+
+}
---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -0,0 +1,18 @@
+package oopWithNLayeredApp.dataAccess;
+
+import java.security.PublicKey;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class HibernateProductDao implements ProductDao {
+
+	public void add(Product product) {
+		System.out.println("Hibernate ile veritabanın eklendi");
+	
+		
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
